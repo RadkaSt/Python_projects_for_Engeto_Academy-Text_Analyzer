dd6acbd6c3674aea1474b227273721c8f45c0c37
RadkaSt <138523795+RadkaSt@users.noreply.github.com>
2024-07-13 12:53:44 +0200

    main_project_1.py

--- project_1.py
+++ project_1.py
@@ -11,7 +11,7 @@ from plot_word_freq import plot_word_freq
 # importing my defined function to plot the freq of word based on the length of the word
 
 #Defining how many texts are in the template and therefore which numbers can be chosen later
-possible_numbers = list(range(1,(len(task_template.TEXTS)+1)))
+possible_numbers = list(range(1, (len(task_template.TEXTS) + 1)))
 
 #Registered users (keys represent names, passwords are in the values)
 registered_users = {
@@ -31,8 +31,8 @@ if user_name not in registered_users:
 elif registered_users[user_name] == password:
     print(f"Welcome to the app,{user_name}")
     text_number = int(input(f"Enter a number btw {possible_numbers[0]} and {possible_numbers[-1]}:"))
-    chosen_text = task_template.TEXTS[text_number-1]
     if text_number in possible_numbers:
+        chosen_text = task_template.TEXTS[text_number-1]
         analyze_text(chosen_text)
         plot_word_freq(chosen_text)
     else:
